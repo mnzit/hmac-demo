35b92ab5de6196de0d3fba20e7597f5eb9879043
= <=>
2020-07-08 21:42:54 +0545

    Initial Logging Configured

---
+++ src/main/java/com/mnzit/hmac/demo/HmacMain.java
@@ -0,0 +1,16 @@
+package com.mnzit.hmac.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author Manjit Shakya <mnzitshakya@gmail.com>
+ */
+@Slf4j
+public class HmacMain {
+
+    public static void main(String[] args) {
+        log.debug("=============Starting HMAC module=============");
+    }
+
+}
